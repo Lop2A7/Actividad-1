3ee3eedeeb22627bfcb7084b9c3c2403304e1e1e
Edson <edson.bejar@ucsm.edu.pe>
2022-08-21 16:45:30 -0500

    Pylab01.java

---
+++ PyLab01.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PyLab01 {
+    public static void main(String[] args){
+        int vida, rp;
+        boolean fl=false;
+        vida  = 0;
+        Scanner sc = new Scanner(System.in);
+        do {
+            vida ++;
+            System.out.println("Vida:"+ vida + "\t ¿Cual es el numero secreto?");
+            rp= sc.nextInt();
+            if (rp==1234)
+                fl=true;
+        }while(vida<3 && !fl);
+        if (fl== true){
+            System.out.println("Adivinaste!!!");
+            System.out.println("Ganaste un celular");
+        }
+        if (fl== false)
+            System.out.println("Ups perdiste");
+    }
+}
