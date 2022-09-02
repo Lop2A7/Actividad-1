99786ed05d02034073dd9221cec283ec76e2b416
Rocio00006 <111361647+Rocio00006@users.noreply.github.com>
2022-09-01 19:42:23 -0500

    Update contador.java

--- P03Métodos de Instancia y de Clase/contador.java
+++ P03Métodos de Instancia y de Clase/contador.java
@@ -2,7 +2,10 @@ package contadortest;
 
 public class Contador {
     static int acumulador = 0;  //variable de clase
+    final static int VALOR_INICIAL = 10;
     private int valor;          //variable de instancia 
+    static int nContadores = 0; //variable de clase
+    static int ultimoContador = 0;  //variable de clase
     
     public static int acumulador() {    //metodo de clase
         return acumulador;
@@ -10,6 +13,12 @@ public class Contador {
     public Contador(int valor){     //metodo de clase      
         this.valor= valor;
         acumulador += valor;
+        nContadores++;
+        ultimoContador = valor;
+    }
+    public Contador(){
+        //new Contador(Contador.VALOR_INICIAL);
+        this(Contador.VALOR_INICIAL);
     }
     public void inc(){              //metodo de instancia
         valor++;
