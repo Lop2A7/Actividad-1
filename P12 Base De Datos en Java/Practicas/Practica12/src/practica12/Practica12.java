473c60291eb9145abd17e87d15c6b400618ea63d
Rocio00006 <111361647+Rocio00006@users.noreply.github.com>
2022-12-01 19:39:57 -0500

    Add files via upload

---
+++ P12 Base De Datos en Java/Practicas/Practica12/src/practica12/Practica12.java
@@ -0,0 +1,26 @@
+package practica12;
+import java.sql.*; 
+
+public class Practica12 {
+
+    public static void main(String args[]) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");     //registrar controlador
+            Connection con = DriverManager.getConnection(
+            "jdbc:mysql://localhost:3307/sonoo", "root", "root");
+            //obetner la conexion
+            //here sonoo is database name, root is username and password 
+            Statement stmt = con.createStatement();
+            
+            ResultSet rs = stmt.executeQuery("select * from personas");
+            
+            while (rs.next()) {
+                System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3));
+            }
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+
